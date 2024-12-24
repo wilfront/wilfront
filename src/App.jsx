d387292afff6498e39e9b42fc3a84e49f39d55ed
wilfront <wilfront@outlook.com>
2024-12-24 01:09:06 -0300

    barbeiro

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from './Components/Header/Header'
 import Home from './pages/Home/Home'
-import About from './pages/About/About'
 import Services from './pages/Services/Services'
 import Contact from './pages/Contact/Contact'
 import Home3 from "./pages/Home3/Home3"
